d8279631a4099e7c0501603bb419345baff16ca8
Bryan McCann <bmac929@yahoo.com>
2019-08-20 20:16:26 -0500

    api call on dashboard

--- next-episode-no-react/public/dashboard.js
+++ next-episode-no-react/public/dashboard.js
@@ -4,54 +4,71 @@ var tvImages = {
 }
 
 $(document).ready(function () {
-    var loggedUser = JSON.parse(sessionStorage.getItem("user"))
-
-    $("#add").on("click", function () {
-        console.log("click");
-        var postData = {
-            show: $("#showSearch").val(),
-            email: loggedUser.email
-        }
-        //console.log(postData);
+    var loggedUser = JSON.parse(sessionStorage.getItem("user"));
+    var userShows = loggedUser.shows;
+    userShows.forEach(function (show) {
+        console.log(show);
         $.ajax({
-            url: "/dashboard",
+            url: "/dashboard/load",
             method: "POST",
-            data: postData
-            //data: user
+            data: show,
         }).then(function (response) {
-            console.log(response);
-            console.log(response.episode.show.epguide_name);
-            console.log(response.episode.season);
-            console.log(response.episode.number);
-            console.log(response.episode.title);
-            console.log(response.episode.release_date);
-            if (response.hasOwnProperty("error")) {
-                alert("show not found")
-            } else {
-                var tRow = $("<tr>");
-
-                // Methods run on jQuery selectors return the selector they we run on
-                // This is why we can create and save a reference to a td in the same statement we update its text
-                var name = $("<td>").text(response.episode.show.epguide_name);
-                var season = $("<td>").text(response.episode.season);
-                var episode = $("<td>").text(response.episode.number);
-                var title = $("<td>").text(response.episode.title);
-                var date = $("<td>").text(response.episode.release_date);
-
-                // Append the newly created table data to the table row
-                tRow.append(name, season, episode, title, date);
-                // Append the table row to the table body
-                $("tbody").append(tRow);
-                //append data to page
-                //postData.show.toLowerCase()
-            }
-
-        });
+            // console.log(response);
+            var tRow = $("<tr>");
 
 
+            var name = $("<td>").text(response.episode.show.epguide_name);
+            var season = $("<td>").text(response.episode.season);
+            var episode = $("<td>").text(response.episode.number);
+            var title = $("<td>").text(response.episode.title);
+            var date = $("<td>").text(response.episode.release_date);
 
-        // console.log($("#email").val());
-        // console.log($("#password").val());
 
+            tRow.append(name, season, episode, title, date);
+
+            $("tbody").append(tRow);
+
+        })
     })
+});
+
+$("#add").on("click", function () {
+    console.log("click");
+    var postData = {
+        show: $("#showSearch").val(),
+        email: loggedUser.email
+    }
+    //console.log(postData);
+    $.ajax({
+        url: "/dashboard",
+        method: "POST",
+        data: postData
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.episode.show.epguide_name);
+        console.log(response.episode.season);
+        console.log(response.episode.number);
+        console.log(response.episode.title);
+        console.log(response.episode.release_date);
+        if (response.hasOwnProperty("error")) {
+            alert("show not found")
+        } else {
+            var tRow = $("<tr>");
+
+            // This is why we can create and save a reference to a td in the same statement we update its text
+            var name = $("<td>").text(response.episode.show.epguide_name);
+            var season = $("<td>").text(response.episode.season);
+            var episode = $("<td>").text(response.episode.number);
+            var title = $("<td>").text(response.episode.title);
+            var date = $("<td>").text(response.episode.release_date);
+
+            // Append the newly created table data to the table row
+            tRow.append(name, season, episode, title, date);
+
+            $("tbody").append(tRow);
+
+        }
+
+    });
+
 });
